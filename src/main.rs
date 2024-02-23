e2ad95712fbadeb59a381c1608991d97b651124b
Monica Giambitto <monica.giambitto@pm.me>
2024-02-23 13:10:15 +0100

    process -c option, return bytes and file name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let options = std::env::args().nth(1);
+    let filename = std::env::args().nth(2).expect("no file given");
+    let file = File::open(filename.clone()).expect("could not open file");
+
+    if options == Some("-c".to_string()) {
+        let byte_length = file.metadata().unwrap().len();
+        println!("{} {}", byte_length, filename);
+        return;
+    }
 }
